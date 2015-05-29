ddaf5cd68718711dbfc7a085e569322e13d5444d
heki1224 <heki1224@gmail.com>
2015-05-30 07:40:26 +0900

    Initial commit

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by heki on 2015/01/15.
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/hello/SampleController.java
@@ -0,0 +1,14 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class SampleController {
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+}
